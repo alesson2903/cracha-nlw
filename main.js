fa13224cd36c6d11f0bb1d50532b8587e81c432a
Alesson_padilha <85697738+alesson2903@users.noreply.github.com>
2021-10-24 15:41:17 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+const LinksSocialMedia = {
+  github: 'alesson2903',
+  youtube: 'alessonTM',
+  facebook: 'alesson.oliveira.39794',
+  instagram: 'alesson.op',
+  twitter: 'Alesson61737609'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      UserImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
